c3c500225cf38d8c54e70679b6e1130370fe4983
Wise <99487616@qq.com>
2016-05-20 14:34:03 +0800

    Initial Commit

---
+++ CategoryUtilsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryUtilsTest
+//
+//  Created by wuruizhi on 5/20/16.
+//  Copyright © 2016 wuruizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CategoryUtilsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CategoryUtilsTest
+//
+//  Created by wuruizhi on 5/20/16.
+//  Copyright © 2016 wuruizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
